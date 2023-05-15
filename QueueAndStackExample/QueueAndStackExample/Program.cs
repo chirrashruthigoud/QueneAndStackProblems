177501bf0f53f56143764f99e8c9ff509de1580d
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-15 17:18:22 +0530

    Add : create stack

---
+++ QueueAndStackExample/QueueAndStackExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueAndStackExample
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+          StackQueue stack = new StackQueue();
+            stack.PushNode(56);
+            stack.PushNode(30);
+            stack.PushNode(70);
+            stack.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QueueAndStackExample/QueueAndStackExample/StackQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace QueueAndStackExample
+{
+    public class StackQueue
+    {
+        public Node top;
+
+        public void StackMethod()
+        {
+            top = null;
+        }
+        public void PushNode(int value)
+        {
+            Node node = new Node(value);
+            if (top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+            Console.WriteLine("Pushed values :" + value);
+        }
+        public void Display()
+        {
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine("stack values : " + temp.data);
+                temp = temp.next;
+            }
+
+        }
+
+    }
+    
+}
